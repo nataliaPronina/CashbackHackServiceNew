186469dfc35a2909a8760a4383eeb9d902d99651
Nata test <verba16@yandex.ru>
2023-11-25 16:34:51 +0300

    test added

---
+++ src/test/java/CashBackHackServiceTest.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+import ru.netology.CashBackHackService;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashBackHackServiceTest {
+    @Test
+    public void CashBackIf980() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 980;
+        int actual = service.remain(amount);
+        int expected = 20;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void CashBackIf1000() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void CashBackIf1200() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1200;
+        int actual = service.remain(amount);
+        int expected = 800;
+
+        assertEquals(actual, expected);
+    }
+}
