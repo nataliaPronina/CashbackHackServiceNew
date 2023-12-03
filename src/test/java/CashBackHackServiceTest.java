45f2bc441a725b53a0d2391a2dc4d619329e0080
Nata test <verba16@yandex.ru>
2023-12-03 17:10:30 +0300

    check

--- src/test/java/CashBackHackServiceTest.java
+++ src/test/java/CashBackHackServiceTest.java
@@ -17,15 +17,15 @@ public class CashBackHackServiceTest {
         assertEquals (expected, actual);
     }
 
-//    @Test
-//    public void CashBackIf1000() {
-//        CashBackHackService service = new CashBackHackService();
-//        int amount = 1000;
-//        int actual = service.remain(amount);
-//        int expected = 0;
-//
-//        assertEquals (expected, actual);
-//    }
+    @Test
+    public void CashBackIf1000() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals (expected, actual);
+    }
 
     @Test
     public void CashBackIf1200() {
